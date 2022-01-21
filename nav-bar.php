dc3910d811a727db3a0e7798055836f4ba39cdc3
Sumir Bhandari <sumir.bhandari11@gmail.com>
2022-01-21 16:42:02 +0545

    added nav-bar signup and style

---
+++ nav-bar.php
@@ -0,0 +1,10 @@
+<div class="header-content">
+    <a class="title">rojgar</a>
+    <div class="nav-bar">
+        <ul class="nav-list">
+            <li class="nav-items"><a href="">Explore </a></li>
+            <li class="nav-items"><a href="">Become freelancer </a></li>
+            <li class="nav-items join-button"><a href="./signup.php">Join </a></li>
+        </ul>
+    </div>
+</div>
---
+++ signup.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <title>Rojgar </title>
+</head>
+
+<body>
+    <div class="container">
+        <?php include 'nav-bar.php' ?>
+        <div class="joinPopup">
+            <form>
+                <input type="text" class="input" placeholder="Email" />
+                <input type="text" class="input" placeholder="Password" />
+            </form>
+        </div>
+
+
+    </div>
+
+
+</body>
+
+</html>
