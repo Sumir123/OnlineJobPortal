31a9b146f9bfbcc8f2c50ac85621def1d6a7c686
Sumir Bhandari <sumir.bhandari11@gmail.com>
2022-01-30 12:13:20 +0545

    small changes

--- employer/register.php
+++ employer/register.php
@@ -1,2 +1,24 @@
-<?php include '../base.php' ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <title>Rojgar </title>
+</head>
+
+<body>
+    <div class="header-content">
+        <a href="../index.php" class="title">rojgar</a>
+        <div class="nav-bar">
+            <ul class="nav-list">
+                <li class="nav-items"><a href="">Explore </a></li>
+                <li class="nav-items"><a href="">Browse Jobs </a></li>
+                <li class="nav-items join-button"><a href="">Find work </a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="container">
 <link rel="stylesheet" href="../style.css">
--- freelancer/register.php
+++ freelancer/register.php
@@ -1,15 +1,37 @@
-<?php include '../base.php' ?>
-<link rel="stylesheet" href="../style.css">
+<!DOCTYPE html>
+<html>
 
-<div class="content">
-    <h1>Register as Freelancer</h1>
-    <form action="">
-        <input type="text" name="username" placeholder="Username" id="username" autocomplete="off"><br>
-        <input type="text" name="email" placeholder="Email" id="email" autocomplete="off"><br>
-        <input type="text" name="phone" placeholder="Phone Number" id="phone" autocomplete="off"><br>
-        <input type="text" name="password" placeholder="Password" id="password" autocomplete="off"><br>
-        <input type="text" name="confirmPassword" placeholder="Confirm Password" id="confirmPassword"><br>
+<head>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-        <input type="submit" value="Register">
-    </form>
-    <div>
+    <title>Rojgar </title>
+</head>
+
+<body>
+    <div class="header-content">
+        <a href="../index.php" class="title">rojgar</a>
+        <div class="nav-bar">
+            <ul class="nav-list">
+                <li class="nav-items"><a href="">Explore </a></li>
+                <li class="nav-items"><a href="">Browse Jobs </a></li>
+                <li class="nav-items join-button"><a href="">Find work </a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="container">
+        <link rel="stylesheet" href="../style.css">
+
+        <div class="content">
+            <h1>Register as Freelancer</h1>
+            <form action="">
+                <input type="text" name="username" placeholder="Username" id="username" autocomplete="off"><br>
+                <input type="text" name="email" placeholder="Email" id="email" autocomplete="off"><br>
+                <input type="text" name="phone" placeholder="Phone Number" id="phone" autocomplete="off"><br>
+                <input type="text" name="password" placeholder="Password" id="password" autocomplete="off"><br>
+                <input type="text" name="confirmPassword" placeholder="Confirm Password" id="confirmPassword"><br>
+
+                <input type="submit" value="Register">
+            </form>
+            <div>
