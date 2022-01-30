c1c806155022b6c3e19eb8f18f54b03d6457a577
Sumir Bhandari <sumir.bhandari11@gmail.com>
2022-01-30 09:34:09 +0545

    updated folder structure and added admin base employer jobseeker

---
+++ admin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Admin Login</h1>
+    <form>
+        <label for="username"> Username: </label> <input type="text" name="username" placeholder="Username" id="username"><br>
+        <label for="password"> Password: </label> <input type="text" name="password" placeholder="password" id="password"><br>
+        <input type="submit" value="Login">
+    </form>
+    <br> <a href="./index.php">Back to home</a>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,32 +1,11 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <link rel="stylesheet" href="./style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-    <title>Rojgar </title>
-</head>
-
-<body>
-    <div class="container">
-        <?php include 'nav-bar.php' ?>
-        <div class="heading">
-            <h4>Hire <i>freelancers</i> for any job, online.</h4>
-            <div class="button-area">
-                <button class="hire-button">Hire a Freelancer</button>
-                <button class="work-button">Work as Freelancer</button>
-            </div>
-        </div>
+<?php include 'base.php' ?>
+<div class="heading">
+    <h4>Hire <i>freelancers</i> for any job, online.</h4>
+    <div class="button-area">
+        <a class="hire-button" href="./employer.php">Hire a Freelancer</a>
+        <a class="work-button" href="./jobseeker.php">Work as Freelancer</a>
+    </div>
+</div>
 </body>
-<script>
-    var join = document.querySelector(".join-tag");
-    join.addEventListener('click', popup())
-
-    function popup(e) {
-        popupContetnt = "";
-    }
-</script>
 
 </html>
